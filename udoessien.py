43af59e035762d74cd70fae7fd3c4eda8f6af1e7
Ubong Udoessien <ubongudoessien@gmail.com>
2020-06-08 00:14:29 +0100

    Add files via upload

---
+++ udoessien.py
@@ -0,0 +1,8 @@
+#global variables
+
+name = "Ubong Udoessien "
+id = "HNG-00523 "
+email = "ubongudoessien@gmail.com "
+language = "Python "
+
+print("Hello World, this is " + name + "with "+ "HNGi7 " + id + "and email " + email + "using "+ language + "for stage 2 task.")
